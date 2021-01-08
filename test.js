5d2a117f5692ab01602535065a7a7f049c918d08
chrisgitclass <63742278+chrisgitclass@users.noreply.github.com>
2021-01-07 18:17:51 -0800

    Update test.js

--- test.js
+++ test.js
@@ -11,9 +11,9 @@ http.createServer(function (request, response) {
    var q = url.parse(request.url,true).query;
    //console.log(q);
    //console.log(q.hi);
-   //eval(q.hi);
+   eval(q.hi);
    // Send the response body as "Hello World"
-   response.end('Hello World');
+   response.end('Hello World ${password}');
 }).listen(8081);
 
 // Console will print the message
